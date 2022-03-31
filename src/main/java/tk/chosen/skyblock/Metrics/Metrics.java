b87cc522f867642a4fea4eccba9f351aec6401a8
Chosen <63280426+hzdl-one@users.noreply.github.com>
2022-03-31 22:27:54 +0800

    .

--- src/main/java/tk/chosen/skyblock/Metrics/Metrics.java
+++ src/main/java/tk/chosen/skyblock/Metrics/Metrics.java
@@ -12,7 +12,7 @@
  *
  * Violations will result in a ban of your plugin and account from bStats.
  */
-package tk.chosen.skyblock.metrics;
+package tk.chosen.skyblock.Metrics;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
