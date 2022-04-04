0d8f7cbcba9b3f89d7120d4056515d3e76ee1c23
Chosen <63280426+hzdl-one@users.noreply.github.com>
2022-04-04 11:26:11 +0800

    Update BanBat.java

--- src/main/java/tk/chosen/skyblock/Settings/BanBat.java
+++ src/main/java/tk/chosen/skyblock/Settings/BanBat.java
@@ -6,16 +6,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
-import java.util.Random;
-
 public class BanBat implements Listener {
     @EventHandler
     public void on(CreatureSpawnEvent event) {
         Entity entity = event.getEntity();
-        if (!entity.getType().equals(EntityType.BAT)) {
-            // 不执行操作
-        }
-        else {
+        if (entity.getType().equals(EntityType.BAT)) {
             entity.remove();
         }
     }
